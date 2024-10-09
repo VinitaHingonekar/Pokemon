093e01cb466f0d16d61c4995b251131baebb69d7
VinitaHingonekar <hingonekarvinita@gmail.com>
2024-10-10 02:44:04 +0530

    Update Main.cpp
    Added an introductory message from Professor Oak, presented 3 Pokémon choices, and confirmed the player's selection with a cout statement.

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 int main() {
-    string player_name;
-    
-    cout << "Enter your name: ";
-    cin >> player_name;
+    	cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+		cout << "You can choose one of the following Pokémon:\n";
+		cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+		cout << "Which Pokémon would you like to choose? Enter the number: ";
 
-    cout << "Great start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
+        int choice;
+        cin >> choice;
+
+        if(choice == 1)
+            cout << "You chose Bulbasaur! A wise choice." << endl;
+        else if(choice == 2)
+            cout << "You chose Charmander! A fiery choice." << endl;
+        else if(choice == 3)
+            cout << "You chose Squirtle! A cool choice." << endl;
+        else
+            cout << "Invalid choice. Please restart the game." << endl;
 
     return 0;
 }

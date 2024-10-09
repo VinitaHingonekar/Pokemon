0430fa31aa267cf9b4f40f2b21a0ef26da1777fe
VinitaHingonekar <hingonekarvinita@gmail.com>
2024-10-10 02:19:31 +0530

    Update Main.cpp
    Added a message asking the player for their name and responding back

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string player_name;
+    
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "Great start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
+
+    return 0;
+}
